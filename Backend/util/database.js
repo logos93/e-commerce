a2bfc61ba8066bc950f9e40374ff617162bd00df
test <test@test.com>
2022-12-15 16:15:32 +0530

    can add product to cart and save to db

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,8 +1,8 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("node-complete", "root", "project_pass@123", {
-  dialect: "mysql",
-  host: "localhost",
+const sequelize = new Sequelize('node-complete', 'root', 'project_pass@123', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
 module.exports = sequelize;
